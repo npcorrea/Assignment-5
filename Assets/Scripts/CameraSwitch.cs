741ac004f1e8f9b2fbc1ab84365c896c26eec29f
Noelle Correa <npcorrea@ucsc.edu>
2019-02-18 12:21:20 -0800

    Added unity project

---
+++ Assets/Scripts/CameraSwitch.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour
+{
+    public Button cam;
+    public Camera fp;
+    public Camera tp;
+    private bool firstPerson;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // start with third person camera
+        firstPerson = false;
+        cam.onClick.AddListener(SwitchOnClick);
+    }
+
+    // when button clicked make firstPerson true/false
+    void SwitchOnClick()
+    {
+        if (firstPerson == false)
+        {
+            firstPerson = true;
+        }
+        else
+        {
+            firstPerson = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // enables third person camera
+        if (firstPerson == false)
+        {
+            tp.enabled = true;
+            fp.enabled = false;
+        }
+        // enables first person camera
+        else
+        {
+            tp.enabled = false;
+            fp.enabled = true;
+        }
+    }
+}
---
+++ Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CharacterControl : MonoBehaviour
+{
+
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private Animator animate;
+
+    private float moveSpeed = 2;
+    private float turnSpeed = 200;
+
+    private float tempV = 0;
+    private float tempH = 0;
+    private readonly float backwards = 0.66f;
+    private bool isGrounded;
+    private int count;
+
+    public GameObject pickupEffect;
+    public GameObject door;
+    public string mainMenuScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        isGrounded = true;
+        count = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animate.SetBool("Grounded", isGrounded);
+
+        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
+
+        if (v < 0)
+        {
+            v *= backwards;
+        }
+
+        tempV = Mathf.Lerp(tempV, v, Time.deltaTime * 10);
+        tempH = Mathf.Lerp(tempH, h, Time.deltaTime * 10);
+
+        transform.position += transform.forward * tempV * moveSpeed * Time.deltaTime;
+        transform.Rotate(0, tempH * turnSpeed * Time.deltaTime, 0);
+
+        animate.SetFloat("MoveSpeed", tempV);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Collectible"))
+        {
+            Instantiate(pickupEffect, other.transform.position, Quaternion.identity);
+            Destroy(other.gameObject);
+            count++;
+
+            if( count == 3)
+            {
+                door.GetComponent<Animation>().Play("DoorOpen");
+            }
+            else if (count == 4)
+            {
+                SceneManager.LoadScene(mainMenuScene);
+            }
+        }
+    }
+}
+
